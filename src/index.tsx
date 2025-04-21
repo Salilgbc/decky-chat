4c52e573c9b301c84d09799b09de3ffd8e2577f6
Salil Sachdeva <salil.sachdeva@georgebrown.ca>
2025-04-21 17:00:45 -0400

    Initial commit: Complete Decky Discord Integration plugin structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,25 @@
-// Discord Integration Plugin...
+import { ButtonItem, PanelSection, PanelSectionRow, staticClasses, DialogButton } from "@decky/ui";
+import { definePlugin, toaster } from "@decky/api";
+import { useState } from "react";
+import { FaDiscord } from "react-icons/fa";
+
+const ServerList = () => {
+  return (
+    <PanelSection title="Discord Servers">
+      <PanelSectionRow>
+        <ButtonItem layout="below" onClick={() => toaster.toast({ title: "Coming Soon", body: "Server list functionality will be implemented" })}>
+          Connect to Discord
+        </ButtonItem>
+      </PanelSectionRow>
+    </PanelSection>
+  );
+};
+
+export default definePlugin(() => {
+  console.log("Discord Integration plugin initializing");
+  return {
+    name: "Discord Integration",
+    content: <ServerList />,
+    icon: <FaDiscord />,
+  };
+});
