507d52a148f7075260fa6a1c75df9f49d1936a42
Salil Sachdeva <salil.sachdeva@georgebrown.ca>
2025-04-21 20:05:39 -0400

    Add configuration and build files

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,25 @@
-import deckyPlugin from "@decky/rollup";
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import typescript from '@rollup/plugin-typescript';
+import { defineConfig } from 'rollup';
 
-export default deckyPlugin({
-  // Add your extra Rollup options here
-})
+export default defineConfig({
+  input: './src/index.tsx',
+  output: {
+    file: 'dist/index.js',
+    format: 'es',
+    sourcemap: true
+  },
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    json(),
+    typescript(),
+    replace({
+      preventAssignment: true,
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ]
+});
